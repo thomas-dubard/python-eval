b4b82c3ec466bc168e07be0e62f045afe9ceb69f
thomas-dubard <55085630+thomas-dubard@users.noreply.github.com>
2020-01-15 13:33:22 +0100

    update1

--- needleman_wunsch/bundle.py
+++ needleman_wunsch/bundle.py
@@ -1,8 +1,34 @@
+import argparse
 from colorama import Fore, Style
 
 def red_text(text):
     return f"{Fore.RED}{text}{Style.RESET_ALL}"
 
 # que l'on peut utiliser comme ceci
-message = "def"
-print(f"abc{red_text(message)}ghi")
+#message = "def"
+#print(f"abc{red_text(message)}ghi")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("dataset", help="Couples de fragments", type=list)
+
+args = parser.parse_args()
+
+swap = args.dataset
+n = len(swap)
+
+if n%2 == 1:
+    raise TypeError("Il faut des couples de fragments")
+
+liste = []
+for k in range(n//2):
+    liste.append(tuple(swap[2 * k], swap[2 * k + 1]))
+
+for k in range(n//2):
+    ruler = Ruler(*liste[k])
+    ruler.compute()
+    d = ruler.distance
+    top, bottom = ruler.report()
+    print(f"====== example # {k} - distance = {d}")
+    print(top)
+    print(bottom)
--- needleman_wunsch/ruler.py
+++ needleman_wunsch/ruler.py
@@ -1,12 +1,32 @@
-class Ruler(self, str1, str2):
-    def __init__(self):
-        pass
+import numpy as np
+
+class Ruler:
+    def __init__(self, str1, str2):
+        self.distance = None
+        self._alignA = ""
+        self._alignB = ""
 
     def compute(self):
-        pass
+
+        #construction de la matrice
+        swap = [0 for _ in range(len(str2 + 1))]
+        mat = np.array([swap for _ in range(len(str1 + 1))])
+
+       #remplissage de la matrice
+        mat[0][:] = [k for k in range(len(str2 + 1))]
+        mat[:][0] = [k for k in range(len(str1 + 1))]
+        for i in range(1, len(str1 + 1)):
+            for j in range(1, len(str2 + 1)):
+                if str1[j] == str2[i]:
+                    s1 = mat[i - 1][j - 1] - 1
+                else:
+                    s1 = mat[i - 1][j - 1] + 1
+                s2 = mat[i][j - 1] + 1
+                s3 = mat[i - 1][j] + 1
+                mat[i][j] = min(s1, s2, s3)
 
     def report(self):
-        return top, bottom
+        return self._alignA, self._alignB
 
     @property
     def distance(self):
@@ -14,4 +34,33 @@ class Ruler(self, str1, str2):
 
     @distance.setter
     def distance():
-        self._distance = None
+        res = 0
+        def S(A, B):
+            if A == B:
+                return -1
+            else:
+                return 1
+        A = str1
+        B = str2
+        i = len(str1)
+        j = len(str2)
+        while i > 0 or j > 0:
+            pos = (i > 0 and j > 0)
+            if pos and mat[i][j] == mat[i - 1][j - 1] + S(A[i], B[j]):
+                "".join(A[i], self._alignA)
+                "".join(B[j], self._alignB)
+                i += -1
+                j += -1
+                if A[i] != B[j]:
+                    res += 1
+            elif i > 0 and mat[i][j] == mat[i - 1][j] + 1:
+                "".join(A[i], self._alignA)
+                "".join("-", self._alignB)
+                i += -1
+                res += 1
+            else:
+                "".join("-", self._alignA)
+                "".join(B[j], self._alignB)
+                j += -1
+                res += 1
+        self._distance = res
